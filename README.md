de3ec836e7715cdc3fabe8f3b34cfc244dab7774
Robert Lucas <100799838+Robert-M-Lucas@users.noreply.github.com>
2024-05-16 01:19:30 +0100

    Added SVG support

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Automatically fill in created files (e.g. pngs) with valid content
 ## Features
 
 - PNGs
+- JPGs
+- SVGs
 
 ## Release Notes
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,10 @@
 // Import the module and reference it with the alias vscode in your code below
 import { lstatSync } from 'fs';
 import * as vscode from 'vscode';
-import { PngHandler } from './handlers/png';
 import { FileHandler } from './FileHandler';
 import { JpgHandler } from './handlers/jpg';
+import { PngHandler } from './handlers/png';
+import { SvgHandler } from './handlers/svg';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,13 +14,16 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 
 	const dispose = vscode.workspace.onDidCreateFiles((event: vscode.FileCreateEvent) => {
+		vscode.window.showInformationMessage(`Event`);
 
 		const handlers: FileHandler[] = [
 			new PngHandler(),
 			new JpgHandler(),
+			new SvgHandler()
 		];
 
 		event.files.map((f) => f.fsPath).filter((f) => lstatSync(f).isFile()).forEach((f: string) => {
+			vscode.window.showInformationMessage(`Files ${f}`);
 			for (const handler of handlers) {
 				if (handler.can_handle(f)) {
 					handler.handle(f);
